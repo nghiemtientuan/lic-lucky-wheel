c11ae4dc814a5ac333a892683ccc3add2b045f34
Tuan Nghiem <tuannghiemtien97@gmail.com>
2024-07-15 11:07:08 +0700

    feat: js save local storage

--- assests/index.js
+++ assests/index.js
@@ -1,84 +1,49 @@
 $(document).ready(function () {
-    let countClicked = 0;
     let angle = 0;
     let spinning = false;
     let spinInterval;
 
     // local storage key
-    const GIFTS_KEY = 'gifts';
     const WHEEL_RESULTS_KEY = 'wheel_results_key';
     const WHEEL_BUTTON_OPTION = 'wheel_button_key';
 
     // default value
-    // const GIFTS_DEFAULT = [
-    //     {
-    //         "id": 1,
-    //         "name": "Gấu LIC",
-    //         "file": null,
-    //         "total": 50,
-    //     },
-    //     {
-    //         "id": 2,
-    //         "name": "Móc khoá Gấu LIC",
-    //         "file": null,
-    //         "total": 50,
-    //     },
-    //     {
-    //         "id": 3,
-    //         "name": "Bình nước",
-    //         "file": null,
-    //         "total": 50,
-    //     },
-    //     {
-    //         "id": 4,
-    //         "name": "Túi Tote",
-    //         "file": null,
-    //         "total": 50,
-    //     },
-    //     {
-    //         "id": 5,
-    //         "name": "Balo",
-    //         "file": null,
-    //         "total": 50,
-    //     },
-    //     {
-    //         "id": 6,
-    //         "name": "Áo phông",
-    //         "file": null,
-    //         "total": 50,
-    //     },
-    //     {
-    //         "id": 7,
-    //         "name": "Tuỳ chọn",
-    //         "file": null,
-    //         "total": 50,
-    //     },
-    // ];
+    const GIFT_BOTTLE = {
+        "id": "bottle",
+        "name": "Bình nước LIC",
+        "total": 50,
+    };
+    const GIFT_KEY_BEAR = {
+        "id": "key_bear",
+        "name": "Móc khoá Gấu LIC",
+        "total": 50,
+    };
+    const GIFT_BACKPACK = {
+        "id": "backpack",
+        "name": "Cặp sách LIC",
+        "total": 50,
+    };
+    const GIFT_BEAR = {
+        "id": "bear",
+        "name": "Gấu LIC",
+        "total": 50,
+    };
+    const GIFT_TOTE_BAG = {
+        "id": "tote_bag",
+        "name": "Túi tote LIC",
+        "total": 50,
+    };
+    const GIFT_T_SHIRT = {
+        "id": "t_shirt",
+        "name": "Áo phông LIC",
+        "total": 50,
+    };
     const WHEEL_OPTION_ONE_CLICK = 1;
     const WHEEL_OPTION_TWO_CLICK = 2;
 
     // get value from local storage
-    // let gifts = localStorage.getItem(GIFTS_KEY) || GIFTS_DEFAULT;
-    let wheelResult = localStorage.getItem(WHEEL_RESULTS_KEY) || [];
+    let wheelResult = JSON.parse(localStorage.getItem(WHEEL_RESULTS_KEY)) || [];
     let wheelButtonOption = localStorage.getItem(WHEEL_BUTTON_OPTION) || WHEEL_OPTION_TWO_CLICK;
-
-    // Khoi tao vong quay voi so luong gift dinh truoc
-    // gifts.map(function (gift) {
-    //     $('#wheel__inner').append('<div class="wheel__sec"></div>');
-    // });
-
-    function caculateGift(position) {
-        countClicked++;
-        if (position <= 36) {
-        } else if (position <= 64) {
-        } else if (position <= 150) {
-        } else if (position <= 210) {
-        } else if (position <= 270) {
-        } else if (position <= 330) {
-        } else {
-        }
-        // $('.congratulation').fadeIn();
-    }
 
     $('#wheel__button').click(function () {
         if (wheelButtonOption == WHEEL_OPTION_TWO_CLICK && !spinning) {
@@ -94,15 +59,90 @@ $(document).ready(function () {
         if (spinning || wheelButtonOption == WHEEL_OPTION_ONE_CLICK) {
             spinning = false;
             clearInterval(spinInterval);
-            const random = Math.floor((Math.random() * 360) + 720);
-            angle += random;
+            angle += getRandomDeg();
             $(".wheel__inner").css("transform", `rotate(${angle}deg)`);
             setTimeout(() => {
                 // Chia lấy dư cho 360 để lấy lượng quay không hoàn thành một vòng 360deg
-                caculateGift(angle % 360);
+                saveGift(angle % 360);
             }, 5000);
         }
     })
+
+    function getRandomDeg() {
+        let random, isNearBorderGift, isMissingGift;
+        do {
+            random = Math.floor((Math.random() * 360) + 720);
+            const randomDeg = (angle + random) % 360;
+
+            // check case near border gift
+            isNearBorderGift = (25 < randomDeg && randomDeg < 35)
+            || (85 < randomDeg && randomDeg < 95)
+            || (145 < randomDeg && randomDeg < 155)
+            || (205 < randomDeg && randomDeg < 215)
+            || (265 < randomDeg && randomDeg < 275)
+            || (325 < randomDeg && randomDeg < 335);
+
+            // check case missing gift
+            isMissingGift = !caculateGift(randomDeg);
+        } while(isNearBorderGift || isMissingGift);
+
+        return random;
+    }
+
+    function caculateGift(position) {
+        let caculatedGift;
+        if (30 <= position && position <= 90) {
+            // Case: key bear
+            caculatedGift = GIFT_KEY_BEAR;
+        } else if (position <= 150) {
+            // Case: backpack
+            caculatedGift = GIFT_BACKPACK;
+        } else if (position <= 210) {
+            // Case: bear
+            caculatedGift = GIFT_BEAR;
+        } else if (position <= 270) {
+            // Case: tote
+            caculatedGift = GIFT_TOTE_BAG;
+        } else if (position <= 330) {
+            // Case: t-shirt
+            caculatedGift = GIFT_T_SHIRT;
+        } else {
+            // Case: bottle
+            caculatedGift = GIFT_BOTTLE;
+        }
+
+        // check exist gift
+        if (caculatedGift) {
+            const existGift = wheelResult.find(function (result) {
+                return result.id === caculatedGift.id;
+            });
+            if (!existGift || (existGift && existGift.total < caculatedGift.total)) {
+                return caculatedGift;
+            }
+        }
+
+        return null;
+    }
+
+    function saveGift(position) {
+        let existGift = caculateGift(position);
+        const existGiftIndex = wheelResult.findIndex(function (result) {
+            return result.id === existGift.id;
+        });
+        if (existGiftIndex >= 0) {
+            // case update
+            wheelResult[existGiftIndex].total++;
+        } else {
+            // case create
+            wheelResult.push({
+                ...existGift,
+                total: 1,
+            });
+        }
+        localStorage.setItem(WHEEL_RESULTS_KEY, JSON.stringify(wheelResult));
+    }
+
+    // $('.congratulation').fadeIn();
 
     $('.congratulation__close').click(function () {
         $('.congratulation').fadeOut();
